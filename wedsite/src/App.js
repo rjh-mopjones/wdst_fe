478cdc269fac69972da8580673ad8e8880675b4f
rjh-mopjones <roryhedderman@gmail.com>
2024-01-04 20:57:09 +0000

    initial commit

---
+++ wedsite/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import './styles/styles.css'; // Import your CSS file
+
+
+
+const App = () => {
+  return (
+      <div>
+        <Navbar />
+        {/* Your main content goes here */}
+      </div>
+  );
+};
+
+export default App;
---
+++ wedsite/src/components/Navbar.js
@@ -0,0 +1,20 @@
+// Navbar.js
+
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#itinerary">Itinerary</a></li>
+                <li><a href="#directions">Directions</a></li>
+                <li><a href="#accommodation">Accommodation</a></li>
+                <li><a href="#registry">Registry</a></li>
+                <li><a href="#rsvp">RSVP</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
