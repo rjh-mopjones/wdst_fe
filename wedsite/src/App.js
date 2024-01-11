68954790684ca83692a032ea687b63c66b5d4c95
rjh-mopjones <roryhedderman@gmail.com>
2024-01-11 22:50:14 +0000

    minimising navbar

--- wedsite/src/App.js
+++ wedsite/src/App.js
@@ -22,9 +22,7 @@ const App = () => {
       // TODO sunset active navbar
       <Router>
           <div className={"page"}>
-              <div className={"divBar"}>
               <Navbar/>
-              </div>
               <div className="content">
                   <Routes>
                       <Route path="/" element={<Home/>} />
--- wedsite/src/components/Navbar.js
+++ wedsite/src/components/Navbar.js
@@ -2,10 +2,33 @@
 
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+const { useState, useEffect } = React;
 
 
 
 const Navbar = () => {
+    const [isVisible, setIsVisible] = useState(true);
+    const [height, setHeight] = useState(0)
+
+    useEffect(() => {
+        window.addEventListener("scroll", listenToScroll);
+        return () =>
+            window.removeEventListener("scroll", listenToScroll);
+    }, [])
+
+    const listenToScroll = () => {
+        let heightToHideFrom = 20;
+        const winScroll = document.body.scrollTop ||
+            document.documentElement.scrollTop;
+        setHeight(winScroll);
+
+        if (winScroll > heightToHideFrom) {
+            isVisible && setIsVisible(false);
+        } else {
+            setIsVisible(true);
+        }
+    };
+
     const activeState = ({ isActive }) => {
         return {
             backgroundColor: isActive ? "#995D6A" : "",
@@ -13,25 +36,31 @@ const Navbar = () => {
         };
     };
     return (
-        <div className={'topBar'}>
-            <div>
-                <ul className={'header'}>
-                    <li>21st June 2025</li>
-                    <li>Treberfydd House, Brecon, Wales</li>
+        <div className={"divBar"}>
+                    {
+                        isVisible
+                        &&
+                <div className={'topBar'}>
+                    <div>
+                        <ul className={'header'}>
+                            <li>21st June 2025</li>
+                            <li>Treberfydd House, Brecon, Wales</li>
+                        </ul>
+                    </div>
+                    <div><h1 className='title'>Rory & Ellie</h1></div>
+                </div>
+            }
+            <nav className={'navb'}>
+                <ul>
+                    <li> <NavLink to='/' style={activeState}>Home</NavLink></li>
+                    <li><NavLink to='/itinerary' style={activeState}>Itinerary</NavLink></li>
+                    <li><NavLink to='/directions' style={activeState}>Directions</NavLink></li>
+                    <li><NavLink to='/accommodation' style={activeState}>Accommodation</NavLink></li>
+                    <li><NavLink to='/registry' style={activeState}>Registry</NavLink></li>
+                    <li><NavLink to='/faqs' style={activeState}>FAQs</NavLink></li>
+                    <li><NavLink to='/rsvp' style={activeState}>RSVP</NavLink></li>
                 </ul>
-            </div>
-            <div><h1 className='title'>Rory & Ellie</h1></div>
-        <nav className={'navb'}>
-            <ul>
-                <li> <NavLink to='/' style={activeState}>Home</NavLink></li>
-                <li><NavLink to='/itinerary' style={activeState}>Itinerary</NavLink></li>
-                <li><NavLink to='/directions' style={activeState}>Directions</NavLink></li>
-                <li><NavLink to='/accommodation' style={activeState}>Accommodation</NavLink></li>
-                <li><NavLink to='/registry' style={activeState}>Registry</NavLink></li>
-                <li><NavLink to='/faqs' style={activeState}>FAQs</NavLink></li>
-                <li><NavLink to='/rsvp' style={activeState}>RSVP</NavLink></li>
-            </ul>
-        </nav>
+            </nav>
         </div>
     );
 };
