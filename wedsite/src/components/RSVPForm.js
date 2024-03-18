02f0f74b6e61d8475620315a3aa5c3727b70bd8b
rjh-mopjones <roryhedderman@gmail.com>
2024-03-18 20:41:18 +0000

    add checkbox for rsvp

--- wedsite/src/components/RSVPForm.js
+++ wedsite/src/components/RSVPForm.js
@@ -3,19 +3,22 @@ import React, { useState } from 'react';
 const FormExample = () => {
     // State to hold form input values
     const [formData, setFormData] = useState({
-        firstName: '',
-        lastName: '',
+        fullName: '',
         email: '',
         mealOption: '',
         song: '',
         message: '',
+        attendance: 'false',
     });
 
     const [additionalFormData, setAdditionalFormData] = useState([]);
+    const [isRsvpActive, setRsvpActive] = useState(false)
 
     // Event handler for input changes
     const handleInputChange = (event) => {
         const { name, value } = event.target;
+        console.log(name)
+        console.log(value)
         setFormData({
             ...formData,
             [name]: value,
@@ -31,8 +34,8 @@ const FormExample = () => {
 
     const handleAddPerson = (event) => {
         let newPersonData = {
-            firstName: '',
-            lastName: '',
+            fullName: '',
+            attendance: '',
             mealOption: '',
         }
         setAdditionalFormData([
@@ -57,24 +60,26 @@ const FormExample = () => {
         console.log('Form submitted:', formData);
     };
 
+    function handleReveal() {
+        setRsvpActive(wasRevealed => !wasRevealed)
+    }
 
     return (
         <div className="rsvp-form-container">
             <h1 className="rsvp-title">RSVP</h1>
-
             <div className="rsvp-form-submit">
                 <form onSubmit={handleSubmit}>
                     <div className="rsvp-row">
                         <div className="rsvp-col-25">
-                            <label>First Name: </label>
+                            <label>Full Name: </label>
                         </div>
                         <div className="rsvp-col-75">
                             <input
                                 type="text"
-                                name="firstName"
-                                value={formData.firstName}
+                                name="fullName"
+                                value={formData.fullName}
                                 onChange={handleInputChange}
-                                placeholder={"Your first name(s)"}
+                                placeholder={"Your full name"}
                                 required="true"
                             />
                         </div>
@@ -82,148 +87,135 @@ const FormExample = () => {
                     <br/>
                     <div className="rsvp-row">
                         <div className="rsvp-col-25">
-                            <label> Last Name: </label>
+                            <label>Are You Coming?: </label>
                         </div>
                         <div className="rsvp-col-75">
                             <input
-                                type="text"
-                                name="lastName"
-                                value={formData.lastName}
+                                onClick={handleReveal}
+                                type="checkbox"
+                                name="attendance"
+                                value={formData.attendance}
                                 onChange={handleInputChange}
-                                placeholder={"Your last name(s)"}
                                 required="true"
                             />
                         </div>
                     </div>
-                    <br/>
-                    <div className="rsvp-row">
-                        <div className="rsvp-col-25">
-                            <label> Meal Option: </label>
-                        </div>
-                        <div className="rsvp-col-75">
-                            <select id="mealOption" name="mealOption">
-                                <option value="1">Option 1</option>
-                                <option value="2">Option 2</option>
-                            </select>
+                    { isRsvpActive && (
+                        <div id="rsvp-reveal">
+                            <br/>
+                                <div className="rsvp-row">
+                                    <div className="rsvp-col-25">
+                                        <label> Meal Option: </label>
+                                    </div>
+                                    <div className="rsvp-col-75">
+                                        <select id="mealOption" name="mealOption">
+                                            <option value="1">Option 1</option>
+                                            <option value="2">Option 2</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <br/>
+                                <div className="rsvp-row">
+                                    <div className="rsvp-col-25">
+                                        <label> Email: </label>
+                                    </div>
+                                    <div className="rsvp-col-75">
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            value={formData.email}
+                                            onChange={handleInputChange}
+                                            placeholder={"Your Email"}
+                                            required="true"
+                                        />
+                                    </div>
+                                </div>
+                            <br/>
+                                <div className="rsvp-row">
+                                    <div className="rsvp-col-25">
+                                        <label> Song Suggestions: </label>
+                                    </div>
+                                    <div className="rsvp-col-75">
+                                        <input
+                                            type="text"
+                                            name="song"
+                                            value={formData.song}
+                                            onChange={handleInputChange}
+                                            placeholder={"Suggest us a song you would like played! (Optional)"}
+                                        />
+                                    </div>
+                                </div>
+                            <br/>
+                                <div className="rsvp-row">
+                                    <div className="rsvp-col-25">
+                                        <label> Additonal Message: </label>
+                                    </div>
+                                    <div className="rsvp-col-75">
+                                        <textarea
+                                            name="message"
+                                            value={formData.message}
+                                            onChange={handleInputChange}
+                                            placeholder={"Any additional questions or messages you want us to know? (Optional)"}
+                                        />
+                                    </div>
+                                </div>
+                            <br/>
+                                <div className="rsvp-row">
+                                    <div className="rsvp-nested-respondents">
+                                        {additionalFormData.map((input, index) => {
+                                                return (
+                                                    <div className="rsvp-nested-respondent-wrapper" key={index}>
+                                                        <div className="rsvp-nested-respondent" key={index}>
+                                                            <div className={"rsvp-delete-person-button-div"}>
+                                                                <button type="button" onClick={handleDeletePerson}
+                                                                        data-index={index} className={"rsvp-delete-person-button"}>
+                                                                    &#10006;
+                                                                </button>
+                                                            </div>
+                                                            <br/>
+                                                            <div className="rsvp-row">
+                                                                <div className="rsvp-col-25">
+                                                                    <label>Full Name: </label>
+                                                                </div>
+                                                                <div className="rsvp-col-75">
+                                                                    <input
+                                                                        type="text"
+                                                                        name="fullName"
+                                                                        value={additionalFormData[index].fullName}
+                                                                        onChange={handleNestedInputChange}
+                                                                        placeholder={"Their full name"}
+                                                                        required="true"
+                                                                        data-index={index}
+                                                                    />
+                                                                </div>
+                                                            </div>
+                                                            <br/>
+                                                            <div className="rsvp-row">
+                                                                <div className="rsvp-col-25">
+                                                                    <label> Meal Option: </label>
+                                                                </div>
+                                                                <div className="rsvp-col-75">
+                                                                    <select id="mealOption" name="mealOption">
+                                                                        <option value="1">Option 1</option>
+                                                                        <option value="2">Option 2</option>
+                                                                    </select>
+                                                                </div>
+                                                            </div>
+                                                            <br/>
+                                                        </div>
+                                                    </div>
+                                            )
+                                        }
+                                    )}
+                            </div>
+                            <div className="rsvp-add-person-button-div">
+                                <button type="button" onClick={handleAddPerson} className="rsvp-add-person-button">I want to
+                                    RSVP for another Person
+                                </button>
+                            </div>
                         </div>
                     </div>
-                    <br/>
-                    <div className="rsvp-row">
-                        <div className="rsvp-col-25">
-                            <label> Email: </label>
-                        </div>
-                        <div className="rsvp-col-75">
-                            <input
-                                type="email"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleInputChange}
-                                placeholder={"Your Email"}
-                                required="true"
-                            />
-                        </div>
-                    </div>
-                    <br/>
-                    <div className="rsvp-row">
-                        <div className="rsvp-col-25">
-                            <label> Song Suggestions: </label>
-                        </div>
-                        <div className="rsvp-col-75">
-                            <input
-                                type="text"
-                                name="song"
-                                value={formData.song}
-                                onChange={handleInputChange}
-                                placeholder={"Suggest us a song you would like played! (Optional)"}
-                            />
-                        </div>
-                    </div>
-                    <br/>
-                    <div className="rsvp-row">
-                        <div className="rsvp-col-25">
-                            <label> Additonal Message: </label>
-                        </div>
-                        <div className="rsvp-col-75">
-                            <textarea
-                                name="message"
-                                value={formData.message}
-                                onChange={handleInputChange}
-                                placeholder={"Any additional questions or messages you want us to know? (Optional)"}
-                            />
-                        </div>
-                    </div>
-                    <br/>
-                    <div className="rsvp-row">
-                        <div className="rsvp-nested-respondents">
-                            {additionalFormData.map((input, index) => {
-                                    return (
-                                        <div className="rsvp-nested-respondent-wrapper" key={index}>
-                                            <div className="rsvp-nested-respondent" key={index}>
-                                                <div className={"rsvp-delete-person-button-div"}>
-                                                    <button type="button" onClick={handleDeletePerson}
-                                                        data-index={index} className={"rsvp-delete-person-button"}>
-                                                        &#10006;
-                                                    </button>
-                                                </div>
-                                                <br/>
-                                                <div className="rsvp-row">
-                                                    <div className="rsvp-col-25">
-                                                        <label>First Name: </label>
-                                                    </div>
-                                                    <div className="rsvp-col-75">
-                                                        <input
-                                                            type="text"
-                                                            name="firstName"
-                                                            value={additionalFormData[index].firstName}
-                                                            onChange={handleNestedInputChange}
-                                                            placeholder={"Your first name(s)"}
-                                                            required="true"
-                                                            data-index={index}
-                                                        />
-                                                    </div>
-                                                </div>
-                                                <br/>
-                                                <div className="rsvp-row">
-                                                    <div className="rsvp-col-25">
-                                                        <label>Last Name: </label>
-                                                    </div>
-                                                    <div className="rsvp-col-75">
-                                                        <input
-                                                            type="text"
-                                                            name="lastName"
-                                                            value={additionalFormData[index].lastName}
-                                                            onChange={handleNestedInputChange}
-                                                            placeholder={"Your last name(s)"}
-                                                            required="true"
-                                                            data-index={index}
-                                                        />
-                                                    </div>
-                                                </div>
-                                                <br/>
-                                                <div className="rsvp-row">
-                                                    <div className="rsvp-col-25">
-                                                        <label> Meal Option: </label>
-                                                    </div>
-                                                    <div className="rsvp-col-75">
-                                                        <select id="mealOption" name="mealOption">
-                                                            <option value="1">Option 1</option>
-                                                            <option value="2">Option 2</option>
-                                                        </select>
-                                                    </div>
-                                                </div>
-                                                <br/>
-                                            </div>
-                                        </div>
-                                    )
-                                }
-                            )}
-                        </div>
-                        <div className="rsvp-add-person-button-div">
-                            <button type="button" onClick={handleAddPerson} className="rsvp-add-person-button">I want to
-                                RSVP for another Person
-                            </button>
-                        </div>
-                    </div>
+                    )}
                     <div className="rsvp-row">
                         <button type="submit">Submit</button>
                     </div>
