b51d3d315e65b8f5c01fa87c1b2cf500ea916408
rjh-mopjones <roryhedderman@gmail.com>
2024-05-04 11:30:55 +0100

    add some faqs

--- wedsite/src/pages/FAQs.js
+++ wedsite/src/pages/FAQs.js
@@ -41,27 +41,44 @@ const FAQs= () => {
                 text:classes.text, control:classes.control}}
             >
                 <Accordion.Item value="item-1" classNames={{text:classes.text}}>
-                    <AccordionControl>Control 1</AccordionControl>
-                    <Accordion.Panel>Panel 1</Accordion.Panel>
+                    <AccordionControl>Is there a Free Bar?</AccordionControl>
+                    <Accordion.Panel>Yes</Accordion.Panel>
                 </Accordion.Item>
 
                 <Accordion.Item value="item-2">
-                    <AccordionControl>Control 2</AccordionControl>
-                    <Accordion.Panel>Panel 2</Accordion.Panel>
+                    <AccordionControl>Are kids allowed?</AccordionControl>
+                    <Accordion.Panel>No</Accordion.Panel>
                 </Accordion.Item>
 
                 <Accordion.Item value="item-3">
-                    <AccordionControl>Control 3</AccordionControl>
-                    <Accordion.Panel>Panel 3 </Accordion.Panel>
+                    <AccordionControl>When should we RSVP by?</AccordionControl>
+                    <Accordion.Panel>11:59pm 14th Feb 2025, we will send reminders</Accordion.Panel>
                 </Accordion.Item>
                 <Accordion.Item value="item-4">
-                    <AccordionControl>Control 4</AccordionControl>
-                    <Accordion.Panel>Panel 4 </Accordion.Panel>
+                    <AccordionControl>What time should we arrive?</AccordionControl>
+                    <Accordion.Panel>Latest 12:00pm 21st June 2025</Accordion.Panel>
                 </Accordion.Item>
                 <Accordion.Item value="item-5">
-                    <AccordionControl>Control 5</AccordionControl>
-                    <Accordion.Panel>Panel 5 </Accordion.Panel>
+                    <AccordionControl>Is the ceremony and reception in the same place?</AccordionControl>
+                    <Accordion.Panel>Yes, there is no separation of venues</Accordion.Panel>
                 </Accordion.Item>
+                <Accordion.Item value="item-6">
+                    <AccordionControl>Is the menu completely vegetarian?</AccordionControl>
+                    <Accordion.Panel>Yes, We are both vegetarian so we have a vegetarian menu</Accordion.Panel>
+                </Accordion.Item>
+                <Accordion.Item value="item-7">
+                    <AccordionControl>Is there a strict dress code?</AccordionControl>
+                    <Accordion.Panel>Just normal wedding smart attire</Accordion.Panel>
+                </Accordion.Item>
+                <Accordion.Item value="item-8">
+                    <AccordionControl>Are there plus 1s?</AccordionControl>
+                    <Accordion.Panel>Mainly for planning reasons, we are not offering +1s up front - if you would like one though, feel free
+                                    to contact us</Accordion.Panel>
+                </Accordion.Item>
+            <Accordion.Item value="item-9">
+                <AccordionControl>How do I get there?</AccordionControl>
+                <Accordion.Panel>Brother, I spent a whole evening making a Directions section</Accordion.Panel>
+            </Accordion.Item>
             </Accordion>
         </div>
     );
